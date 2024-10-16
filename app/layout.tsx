11a141ad1e8e003ad1a8880407805a5c646b599a
youba-experiences <social@youba.io>
2024-10-16 18:35:02 +0100

    lint error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,9 @@
 import type { Metadata } from "next";
-import { Roboto, Lato, Open_Sans, Montserrat, Playfair_Display } from "next/font/google";
 import "./globals.css";
 
-const one = Roboto({
-  weight: ['400', '700'],
-  subsets: ["latin"],
-  display: 'swap',
-});
-
-const two = Lato({
-  weight: ['400', '700'],
-  subsets: ["latin"],
-  display: 'swap',
-});
-
-const three = Open_Sans({
-  subsets: ["latin"],
-  display: 'swap',
-});
-
-const four = Montserrat({
-  subsets: ["latin"],
-  display: 'swap',
-});
-
-const five = Playfair_Display({
-  subsets: ["latin"],
-  display: 'swap',
-});
 
 // Choose the font you want to use here
-const chosenFont = five; // two good
+
 
 export const metadata: Metadata = {
   title: "Window Renovation and Painting Services",
